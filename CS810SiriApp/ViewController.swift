1a9e546678865a630a0adc78e4a32da315dbe595
Zachary Donato <zdonato@stevens.edu>
2016-11-10 11:49:46 -0500

    Initial commit of app.

---
+++ CS810SiriApp/ViewController.swift
@@ -0,0 +1,150 @@
+//
+//  ViewController.swift
+//  CS810SiriApp
+//
+//  Created by Zachary Donato on 11/10/16.
+//  Copyright © 2016 CS810IoT. All rights reserved.
+//
+
+import UIKit
+import Speech
+
+class ViewController: UIViewController, SFSpeechRecognizerDelegate {
+
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var myTextView: UITextView!
+    @IBOutlet weak var microphoneButton: UIButton!
+
+    // Speech recognizer object.
+    private let speechRecognizer = SFSpeechRecognizer(locale: Locale.init(identifier: "en-US"));
+    
+    // Used for implementation of speech recognition.
+    private var recognitionRequest: SFSpeechAudioBufferRecognitionRequest?
+    private var recognitionTask: SFSpeechRecognitionTask?
+    private let audioEngine = AVAudioEngine()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        microphoneButton.isEnabled = false;
+        speechRecognizer?.delegate = self;
+        
+        // Request for authorization of the device
+        SFSpeechRecognizer.requestAuthorization{ (authStatus) in
+            var isButtonEnabled = false;
+            switch authStatus {
+            
+            case .authorized:
+                isButtonEnabled = true
+            
+            
+            case .denied:
+                isButtonEnabled = false
+                print("User denied access to speech recognition")
+            
+            case .restricted:
+                isButtonEnabled = false
+                print("Speech recognition restricted on this device")
+            
+            case .notDetermined:
+                isButtonEnabled = false
+                print("Speech recognition not yet authorized")
+            }
+            
+            OperationQueue.main.addOperation() {
+                self.microphoneButton.isEnabled = isButtonEnabled;
+            }
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func microphoneTapped(_ sender: AnyObject) {
+        if audioEngine.isRunning {
+            audioEngine.stop()
+            recognitionRequest?.endAudio()
+            microphoneButton.isEnabled = false
+            microphoneButton.setTitle("Start Recording", for: .normal)
+        } else {
+            startRecording()
+            microphoneButton.setTitle("Stop Recording", for: .normal)
+        }
+    }
+
+    func startRecording() {
+        
+        if recognitionTask != nil {
+            recognitionTask?.cancel()
+            recognitionTask = nil
+        }
+        
+        let audioSession = AVAudioSession.sharedInstance()
+        
+        do {
+            try audioSession.setCategory(AVAudioSessionCategoryRecord)
+            try audioSession.setMode(AVAudioSessionModeMeasurement)
+            try audioSession.setActive(true, with: .notifyOthersOnDeactivation)
+        } catch {
+            print("audioSession properties weren't set because of an error.")
+        }
+        
+        recognitionRequest = SFSpeechAudioBufferRecognitionRequest()
+        
+        guard let inputNode = audioEngine.inputNode else {
+            fatalError("Audio engine has no input node")
+        }
+        
+        guard let recognitionRequest = recognitionRequest else {
+            fatalError("Unable to create an SFSpeechAudioBufferRecognitionRequest object")
+        }
+        
+        recognitionRequest.shouldReportPartialResults = true
+        
+        recognitionTask = speechRecognizer?.recognitionTask(with: recognitionRequest, resultHandler: { (result, error) in
+            var isFinal = false
+            
+            if result != nil {
+                self.myTextView.text = result?.bestTranscription.formattedString
+                isFinal = (result?.isFinal)!
+            }
+            
+            if error != nil || isFinal {
+                self.audioEngine.stop()
+                inputNode.removeTap(onBus: 0)
+                
+                self.recognitionRequest = nil
+                self.recognitionTask = nil
+                
+                self.microphoneButton.isEnabled = true
+            }
+        })
+        
+        let recordingFormat = inputNode.outputFormat(forBus: 0)
+        inputNode.installTap(onBus: 0, bufferSize: 1024, format:recordingFormat) { (buffer, when) in
+            self.recognitionRequest?.append(buffer)
+        }
+        
+        audioEngine.prepare()
+        
+        do {
+            try audioEngine.start()
+        } catch {
+            print("audioEngine coudln't start because of an error.")
+        }
+        
+        myTextView.text = "Say something, I'm listening!"
+    }
+   
+    func speechRecognizer(_ speechRecognizer: SFSpeechRecognizer, availabilityDidChange available: Bool) {
+        if available {
+            microphoneButton.isEnabled = true
+        } else {
+            microphoneButton.isEnabled = false
+        }
+    }
+}
+
---
+++ CS810SiriAppTests/CS810SiriAppTests.swift
@@ -0,0 +1,36 @@
+//
+//  CS810SiriAppTests.swift
+//  CS810SiriAppTests
+//
+//  Created by Zachary Donato on 11/10/16.
+//  Copyright © 2016 CS810IoT. All rights reserved.
+//
+
+import XCTest
+@testable import CS810SiriApp
+
+class CS810SiriAppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
